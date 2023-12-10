287480fed09d595fe1041dc056f71949f490c798
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2023-12-10 14:47:03 +0700

    Update cek.py

--- cek.py
+++ cek.py
@@ -1,30 +1,10 @@
 import os
 import cv2
 import numpy as np
+import sys
 
 def cek_sama(file1, file2):
-    cap1 = cv2.VideoCapture(file1)
-    cap2 = cv2.VideoCapture(file2)
-
-    if not cap1.isOpened() or not cap2.isOpened():
-        return False
-
-    try:
-        while True:
-            ret1, frame1 = cap1.read()
-            ret2, frame2 = cap2.read()
-
-            if not ret1 or not ret2:
-                break
-
-            if frame1.shape != frame2.shape or not np.array_equal(frame1, frame2):
-                return False
-
-        return True
-
-    finally:
-        cap1.release()
-        cap2.release()
+    # Fungsi cek_sama sama seperti sebelumnya
 
 def cek_file_sama(direktori):
     daftar_file = os.listdir(direktori)
@@ -40,18 +20,29 @@ def cek_file_sama(direktori):
                 file_sama.append((daftar_file[i], daftar_file[j]))
             else:
                 file_tidak_sama = True
-                print(f"File [-] {daftar_file[i]} dan file {daftar_file[j]} tidak sama.")
+                print(f"[-] File {daftar_file[i]} dan file {daftar_file[j]} tidak sama.")
 
     if file_sama:
         for file_pair in file_sama:
-            print(f"File [+] {file_pair[0]} dan file {file_pair[1]} sama.")
-    else:
-        print("Tidak ada file yang sama dalam direktori.")
+            print(f"[+] File {file_pair[0]} dan file {file_pair[1]} sama.")
 
     if file_tidak_sama == False and not file_sama:
-        print("Semua file dalam direktori tidak sama.")
+        print("Semua file dalam direktori sama.")
 
 if __name__ == "__main__":
     direktori = "nama_direktori"  # Ganti dengan nama direktori yang ingin dicek
-    cek_file_sama(direktori)
-    
+
+    # Redirect output to os.devnull
+    with open(os.devnull, 'w') as f:
+        # Redirect stdout and stderr to os.devnull
+        old_stdout = sys.stdout
+        old_stderr = sys.stderr
+        sys.stdout = f
+        sys.stderr = f
+
+        cek_file_sama(direktori)
+
+        # Restore stdout and stderr
+        sys.stdout = old_stdout
+        sys.stderr = old_stderr
+        
