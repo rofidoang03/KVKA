cdda13ca568bff40ffe58654b72f719170109078
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2023-12-10 14:55:52 +0700

    Update cek.py

--- cek.py
+++ cek.py
@@ -35,18 +35,55 @@ class VideoFileChecker(QMainWindow):
             self.check_files(directory)
 
     def cek_sama(self, file1, file2):
-        # Fungsi cek_sama sama seperti sebelumnya
+        cap1 = cv2.VideoCapture(file1)
+        cap2 = cv2.VideoCapture(file2)
+
+        if not cap1.isOpened() or not cap2.isOpened():
+            return False
+
+        try:
+            while True:
+                ret1, frame1 = cap1.read()
+                ret2, frame2 = cap2.read()
+
+                if not ret1 or not ret2:
+                    break
+
+                if frame1.shape != frame2.shape or not np.array_equal(frame1, frame2):
+                    return False
+
+            return True
+
+        finally:
+            cap1.release()
+            cap2.release()
 
     def cek_file_sama(self, direktori):
-        # Fungsi cek_file_sama sama seperti sebelumnya
+        daftar_file = os.listdir(direktori)
+        file_sama = []
+        file_tidak_sama = False
 
-    def check_files(self, directory):
-        self.result_text.clear()
-        self.result_text.append(f"Memeriksa file di: {directory}\n\n")
-        self.cek_file_sama(directory)
+        for i in range(len(daftar_file)):
+            for j in range(i + 1, len(daftar_file)):
+                file1 = os.path.join(direktori, daftar_file[i])
+                file2 = os.path.join(direktori, daftar_file[j])
+
+                if self.cek_sama(file1, file2):
+                    file_sama.append((daftar_file[i], daftar_file[j]))
+                else:
+                    file_tidak_sama = True
+                    self.result_text.append(f"File [-] {daftar_file[i]} dan file {daftar_file[j]} tidak sama.")
+
+        if file_sama:
+            for file_pair in file_sama:
+                self.result_text.append(f"File [+] {file_pair[0]} dan file {file_pair[1]} sama.")
+
+        if file_tidak_sama == False and not file_sama:
+            self.result_text.append("Semua file dalam direktori sama.")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = VideoFileChecker()
     window.show()
     sys.exit(app.exec_())
+                        
